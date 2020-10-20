d2b220d41634546b2f4d71430b4682d28709cf99
Brianna Guest <brianna.guest20@gmail.com>
2020-10-20 19:30:23 -0400

    added utility functions

--- Infrastructure_As_Code/utility.ps1
+++ Infrastructure_As_Code/utility.ps1
@@ -16,5 +16,23 @@ $vswitch = New-VirtualSwitch -VMHost $esx_host -Name $switchName -ErrorAction Ig
 $vport = New-VirtualPortGroup -VirtualSwitch $vswitch -Name $switchName
 
 function createSwitch {
-    
+    Get-VMHost | Format-Table Name
+    $vmhost = Read-Host "Enter the VM Host to place the Switch"
+
+    $switchName = Read-Host "Enter Name for Switch"
+
+    New-VirtualSwitch -VMHost $vmhost -Name $switchName -ErrorAction Ignore
+}
+
+function createPortGroup {
+    Get-VMHost | Format-Table Name
+    $vmhost = Read-Host "Enter the VM Host to place the Port Group"
+
+    $portName = Read-Host "Enter Name for Port"
+
+    Write-Host "Here are the Current Virtual Switches on the VM Host:"
+    Get-VirtualSwitch | Format-Table Name
+    $vswitch = "Enter the Switch to place the Port Group"
+
+    New-VirtualPortGroup -VMHost $vmhost -VirtualSwitch $vswitch -Name $portName
 }
